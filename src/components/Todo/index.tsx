08f04ff32b37685e682b976196fe571c2e386fac
genazoz <40898799+genazoz@users.noreply.github.com>
2022-08-07 23:14:32 +0300

    - Внесены небольшие правки по дизайну

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -98,7 +98,7 @@ const Container = styled.div`
   border-bottom: 1px solid ${props => props.theme.TERTIARY_BACKGROUND_COLOR};
 
   @media (max-width: ${commonTheme.media.tab}) {
-    width: calc(100% - 40px - 15px);
+    width: calc(100% - 45px - 15px);
   }
 `
 const Wrapper = styled.div<{ isEdited: boolean }>`
